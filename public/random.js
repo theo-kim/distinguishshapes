6fb762b875c1647cff5f245bb28679d82d2ceb28
suhan1996 <hs2657@nyu.edu>
2018-04-03 10:20:52 +0800

    new

--- public/random.js
+++ public/random.js
@@ -1,14 +1,19 @@
 /**
  * Created by Suhan on 07/06/2017.
  */
-var obj={};
+var obj={
+    A1:0,
+    set:function (x) {
+        obj[x] = 1;
+        // console.log(obj.x);
+    },
+    print:function () {
+        console.log(obj.A1);
+    }
 
-// console.log(((now.getMilliseconds()).toString().slice(0,3)).slice(1))
-// setInterval(function ()
-// { var now = new Date();
-//     console.log((now.getMilliseconds()).toString());
-//     console.log((now.getMilliseconds()).toString().length)
-//
-// },1);
+}
 
-console.log(parseInt('015'));
+obj.set('A1');
+obj.print();
+
+console.log(obj.A1);
